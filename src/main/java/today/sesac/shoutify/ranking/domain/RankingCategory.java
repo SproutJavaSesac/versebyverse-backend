d618cb73984162151faccb71f82ce431d338bbef
Sooamazing <honang515@gmail.com>
2025-06-13 00:56:46 +0900

    [#6]Fix(ranking): 잘못된 ranking category 수정

--- src/main/java/today/sesac/shoutify/ranking/domain/RankingCategory.java
+++ src/main/java/today/sesac/shoutify/ranking/domain/RankingCategory.java
@@ -5,8 +5,9 @@ import lombok.Getter;
 @Getter
 public enum RankingCategory {
 	SWEAR_SCORE(0, "욕설 점수"),
-	WEEKLY(1, "주간"),
-	MONTHLY(2, "월간");
+	REACTION(1, "반응 수"),
+	POST(2, "게시글 수"),
+	;
 
 	private final int code;
 	private final String description;
