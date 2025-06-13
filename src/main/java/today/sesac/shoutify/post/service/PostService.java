2aa2e76ae82d7669b8f2817ec89fc798fc782c03
wosyh18 <wosyh18@naver.com>
2025-06-13 16:49:06 +0900

    [#13] chore(게시판): 어노테이션 추가

--- src/main/java/today/sesac/shoutify/post/service/PostService.java
+++ src/main/java/today/sesac/shoutify/post/service/PostService.java
@@ -1,7 +1,11 @@
 package today.sesac.shoutify.post.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 import today.sesac.shoutify.post.repository.PostRespository;
 
+@Service
+@RequiredArgsConstructor
 public class PostService {
     private PostRespository postRespository;
 
