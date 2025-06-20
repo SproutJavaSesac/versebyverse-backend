92fa44173dd8eda0b829ad1965a1841f86613218
prac2317 <lowdy92@naver.com>
2025-06-21 07:53:14 +0900

    [#36]Feat(member): MemberService 1차 구현(create, read 초기 구현), 예외 처리 초기 구현

--- src/main/java/today/sesac/shoutify/member/service/MemberService.java
+++ src/main/java/today/sesac/shoutify/member/service/MemberService.java
@@ -1,12 +1,32 @@
 package today.sesac.shoutify.member.service;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import today.sesac.shoutify.member.entity.Member;
+import today.sesac.shoutify.member.entity.RoleType;
+import today.sesac.shoutify.member.entity.SocialType;
+import today.sesac.shoutify.member.exception.MemberNotFoundException;
 import today.sesac.shoutify.member.repository.MemberRepository;
 
 @Service
 @RequiredArgsConstructor
 public class MemberService {
 
-    private final MemberRepository memberRepository;
+	private final MemberRepository memberRepository;
+
+	/**
+	 * TODO: 서비스와 나머지(ex.controller) 사이도 DTO로 통신하기?
+	 */
+	public void createMember(RoleType roleType, SocialType socialType, String nickname) {
+		Member member = Member.create(roleType, socialType, nickname);
+		memberRepository.save(member);
+	}
+
+	public Member findById(Long memberId) {
+		Member member = memberRepository.findById(memberId).orElseThrow(
+			() -> new MemberNotFoundException(String.valueOf(memberId), "해당 id를 가진 회원을 찾을 수 없습니다."));
+		return member;
+	}
+
 }
