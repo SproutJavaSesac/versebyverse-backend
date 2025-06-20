ec9dc44a6bde6a6eb5b0e5e20c6dbdf3c54d1bf2
prac2317 <lowdy92@naver.com>
2025-06-21 05:47:15 +0900

    [#36]Chore(auth): 스프링 시큐리티 filterChain 설정 추가

---
+++ src/main/java/today/sesac/shoutify/auth/config/SecurityConfig.java
@@ -0,0 +1,64 @@
+package today.sesac.shoutify.auth.config;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+/**
+ * TODO: SecurityConfig의 위치 도메인 패키지 안에서 config 패키지를 만들고 넣을지, 공통 패키지를 안에 config 패키지를 만들지 논의 필요
+ */
+@Configuration
+public class SecurityConfig {
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http
+			.cors(cors -> cors.configurationSource(corsConfigurationSource()))
+			.csrf(AbstractHttpConfigurer::disable)
+			.formLogin(AbstractHttpConfigurer::disable)
+			.httpBasic(AbstractHttpConfigurer::disable)
+			.authorizeHttpRequests(auth -> auth
+				.requestMatchers(
+					"/api/oauth2/authorization",
+					"/login/oauth2/code/*"
+				).permitAll()
+				.anyRequest().authenticated()
+			)
+			.oauth2Login(oauth2 -> oauth2
+				.authorizationEndpoint(oAuth2 -> oAuth2
+					.baseUri("/api/oauth2/authorization"))
+				.redirectionEndpoint(oAuth2 -> oAuth2
+					.baseUri("/login/oauth2/code/*"))
+			)
+		;
+
+		return http.build();
+	}
+
+	/**
+	 * TODO: CORS 설정 시큐리티 설저 안에서 정할지, 아니면 따로 팔지 고민 필요
+	 */
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+
+		configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+		configuration.setAllowedHeaders(List.of("*"));
+		configuration.setAllowCredentials(true);
+		configuration.setMaxAge(3600L);
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+
+		return source;
+	}
+}
