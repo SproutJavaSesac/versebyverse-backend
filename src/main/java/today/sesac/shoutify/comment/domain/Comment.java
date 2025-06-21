8f4db6f3a9799ed7f5139ff22bdc84864d4f4ea0
ddol9 <drddol9@gmail.com>
2025-06-21 10:16:11 +0900

    [#37]Feat(comments): 댓글 엔티티 수정

--- src/main/java/today/sesac/shoutify/comment/domain/Comment.java
+++ src/main/java/today/sesac/shoutify/comment/domain/Comment.java
@@ -1,52 +1,73 @@
 package today.sesac.shoutify.comment.domain;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import today.sesac.shoutify.global.domain.BaseEntityOnlyCreatedAt;
+import today.sesac.shoutify.global.domain.BaseEntity;
+import today.sesac.shoutify.post.entity.Post;
 
 @Getter
-@NoArgsConstructor(access= AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 @Table(name = "comments")
-public class Comment extends BaseEntityOnlyCreatedAt {
+public class Comment extends BaseEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    @Column(length = 5000, nullable = false)
-    private String contents;
+	/**
+	 * AI 변환 전 사용자가 입력한 원본 댓글
+	 */
+	@Column(length = 500, nullable = false)
+	private String beforeContents;
 
-    @Column(nullable = false)
-    private int score;
+	/**
+	 * AI가 변환한 후의 댓글 내용
+	 */
+	@Column(length = 500, nullable = false)
+	private String afterContents;
 
-    @Column(nullable = false)
-    private int level;
+	/**
+	 * 댓글이 작성된 게시물
+	 */
+	@ManyToOne
+	@JoinColumn(name = "post_id")
+	private Post post;
 
-    @Column(nullable = false, columnDefinition = "TINYINT(1)")
-    private boolean isDeleted = false;
+	// @ManyToOne
+	// @JoinColumn(name = "member_id")
+	// private Member author;
 
-    @Column(nullable = false, columnDefinition = "TINYINT(1)")
-    private boolean isReported = false;
+	/**
+	 * TODO: 대댓글 기능 추가 예정
+	 */
+	@Column(nullable = false)
+	private int level;
 
-    private Comment(
-            String contents,
-            int score,
-            int level
-    ) {
-        this.contents = contents;
-        this.score = score;
-        this.level = level;
-    }
+	@Column(nullable = false, columnDefinition = "TINYINT(1)")
+	private boolean isDeleted = false;
 
-    public static Comment create(
-            String contents,
-            int score,
-            int level
-    ) {
-        return new Comment(contents, score, level);
-    }
+	@Column(nullable = false, columnDefinition = "TINYINT(1)")
+	private boolean isReported = false;
+
+	private Comment(String beforeContents, String afterContents, Post post) {
+		this.beforeContents = beforeContents;
+		this.afterContents = afterContents;
+		this.post = post;
+		this.level = 0;
+	}
+
+	public static Comment create(String beforeContents, String afterContents, Post post) {
+		return new Comment(beforeContents, afterContents, post);
+	}
 }
 
