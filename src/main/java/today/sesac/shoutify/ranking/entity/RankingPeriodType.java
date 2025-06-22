9d401ad58e3aac19809888bcb68156c4a9450757
Sooamazing <honang515@gmail.com>
2025-06-22 21:23:17 +0900

    [#6]Refactor(ranking): 변경된 ERD에 맞춰 Entity 수정, enum 변수명 displayName으로 수정
    - 코드에서 Enum, db에서는 tinyint 사용 -> 모두 코드, db 둘 다 enum으로 통일
    - enum 필드명 화면에 보여주기 위한 용도라displayName으로 수정.

--- src/main/java/today/sesac/shoutify/ranking/entity/RankingPeriodType.java
+++ src/main/java/today/sesac/shoutify/ranking/entity/RankingPeriodType.java
@@ -3,15 +3,15 @@ package today.sesac.shoutify.ranking.entity;
 import lombok.Getter;
 
 @Getter
-public enum RankingCategory {
-  POST("게시글 수");
+public enum RankingPeriodType {
+  DAILY("일간");
 
   /**
    * 화면에 표시되는 이름입니다.
    */
   private final String displayName;
 
-  RankingCategory(String displayName) {
+  RankingPeriodType(String displayName) {
     this.displayName = displayName;
   }
 }
