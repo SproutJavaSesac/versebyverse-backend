fcdd7360d962ae54504ee6a1c369778196402263
wosyh18 <wosyh18@naver.com>
2025-06-24 18:04:04 +0900

    [#40]chore(게시글): 불필요한 응답 내용 삭제

--- src/main/java/today/sesac/shoutify/post/dto/response/PostCreateResponse.java
+++ src/main/java/today/sesac/shoutify/post/dto/response/PostCreateResponse.java
@@ -4,13 +4,11 @@ import lombok.Getter;
 
 @Getter
 public class PostCreateResponse {
-    private final boolean success;
     private final Long postId;
     private final String title;
     private final String afterContents;
 
-    public PostCreateResponse(boolean success, Long postId, String title, String afterContents) {
-        this.success = success;
+    public PostCreateResponse(Long postId, String title, String afterContents) {
         this.postId = postId;
         this.title = title;
         this.afterContents = afterContents;
@@ -21,13 +19,13 @@ public class PostCreateResponse {
      */
     public static PostCreateResponse createSuccess(Long postId, String title,
                                                    String afterContents) {
-        return new PostCreateResponse(true, postId, title, afterContents);
+        return new PostCreateResponse(postId, title, afterContents);
     }
 
     /**
      * 실패응답
      */
     public static PostCreateResponse failure() {
-        return new PostCreateResponse(false, null, null, null);
+        return new PostCreateResponse(null, null, null);
     }
 }
