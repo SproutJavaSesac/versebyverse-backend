5d11582d3c9120eb91be07579b27fa0f0e79325c
ddol9 <drddol9@gmail.com>
2025-06-25 03:24:44 +0900

    [#43]Feat(comment): 댓글 create 더미데이터 테스트용 코드 작성

--- src/main/java/today/sesac/shoutify/comment/service/CommentService.java
+++ src/main/java/today/sesac/shoutify/comment/service/CommentService.java
@@ -9,8 +9,7 @@ import today.sesac.shoutify.comment.dto.CommentResponse;
 import today.sesac.shoutify.comment.entity.Comment;
 import today.sesac.shoutify.comment.repository.CommentRepository;
 import today.sesac.shoutify.member.entity.Member;
-import today.sesac.shoutify.member.entity.RoleType;
-import today.sesac.shoutify.member.entity.SocialType;
+import today.sesac.shoutify.member.repository.MemberRepository;
 import today.sesac.shoutify.post.entity.Post;
 import today.sesac.shoutify.post.repository.PostRespository;
 
@@ -20,6 +19,7 @@ public class CommentService {
 
 	private final CommentRepository commentRepository;
 	private final PostRespository postRepository;
+	private final MemberRepository memberRepository; //변경 예정
 
 	/**
 	 * 댓글을 생성하고 저장합니다.
@@ -31,13 +31,15 @@ public class CommentService {
 	 */
 	@Transactional
 	public CommentResponse createComment(Long postId, Long authorId, CommentRequest request) {
+
 		// 게시글 존재 확인
 		Post post = postRepository.findById(postId)
 			.orElseThrow(() -> new IllegalArgumentException("게시글이 존재하지 않습니다."));
 
 		// 실제 서비스에서는 주석 처리된 코드로 대체
 		// Member author = memberService.findById(authorId);
-		Member author = Member.create(RoleType.ROLE_USER, SocialType.GOOGLE, "test");
+		Member author = memberRepository.findById(authorId)
+			.orElseThrow(() -> new IllegalArgumentException("회원이 존재하지 않습니다."));
 
 		// 댓글 객체 생성
 		Comment comment = Comment.create(
