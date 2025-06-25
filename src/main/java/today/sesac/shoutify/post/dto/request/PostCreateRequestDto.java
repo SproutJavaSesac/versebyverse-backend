ed83b8bfc3343257e8438a1e617d10e96fe276bb
wosyh18 <wosyh18@naver.com>
2025-06-25 17:59:48 +0900

    [#40]chore(게시글): dto 주석 추가

--- src/main/java/today/sesac/shoutify/post/dto/request/PostCreateRequestDto.java
+++ src/main/java/today/sesac/shoutify/post/dto/request/PostCreateRequestDto.java
@@ -7,14 +7,34 @@ import org.hibernate.validator.constraints.Length;
 
 @Getter
 public class PostCreateRequestDto {
+    /**
+     * 게시물 제목
+     */
     @NotBlank
     private String title;
+
+    /**
+     * 컨셉 타입
+     */
     @NotNull
     private String conceptType;
+
+    /**
+     * 감정 타입
+     * null일 경우 ai가 감정 분석을 해줍니다.
+     */
     private String emotionType;
+
+    /**
+     * 변환 전 사용자가 쓴 게시물 내용
+     */
     @NotBlank
     @Length(max = 1000)
     private String content;
+
+    /**
+     * 이미지 URL
+     */
     private String imageUrl;
 
     public PostCreateRequestDto() {
