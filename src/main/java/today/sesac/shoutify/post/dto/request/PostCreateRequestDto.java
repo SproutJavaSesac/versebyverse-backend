d06aa307390f2e391c659a240753470b2990787a
wosyh18 <wosyh18@naver.com>
2025-06-25 17:55:34 +0900

    [#40]chore(게시글): 게시물 작성시 null 검증 추가

--- src/main/java/today/sesac/shoutify/post/dto/request/PostCreateRequestDto.java
+++ src/main/java/today/sesac/shoutify/post/dto/request/PostCreateRequestDto.java
@@ -1,12 +1,19 @@
 package today.sesac.shoutify.post.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
+import org.hibernate.validator.constraints.Length;
 
 @Getter
 public class PostCreateRequestDto {
+    @NotBlank
     private String title;
+    @NotNull
     private String conceptType;
     private String emotionType;
+    @NotBlank
+    @Length(max = 1000)
     private String content;
     private String imageUrl;
 
