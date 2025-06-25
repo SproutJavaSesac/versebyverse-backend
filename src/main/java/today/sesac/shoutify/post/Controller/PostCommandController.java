a3c8f7afb6fa185ffd168df0ef4ff5cc2b976717
wosyh18 <wosyh18@naver.com>
2025-06-25 15:18:13 +0900

    [#40]chore(게시글): ResponseEntity 삭제

--- src/main/java/today/sesac/shoutify/post/Controller/PostCommandController.java
+++ src/main/java/today/sesac/shoutify/post/Controller/PostCommandController.java
@@ -1,7 +1,6 @@
 package today.sesac.shoutify.post.Controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,37 +23,37 @@ public class PostCommandController {
      * 게시물 작성
      */
     @PostMapping
-    public ResponseEntity<ApiResponse<PostCreateResponse>> savePost(
+    public ApiResponse<PostCreateResponse> savePost(
             @RequestBody PostCreateRequest request) {
         Long memberId = 1L; //현재 사용자 memberId 1로 하드코딩 추후 변경 예정
         PostCreateResponse response = postCommandService.savePost(request, memberId);
-        return ResponseEntity.ok(ApiResponse.success(response));
+        return ApiResponse.success(response);
     }
 
     /**
      * 게시물삭제
      */
     @DeleteMapping("/{postId}")
-    public ResponseEntity<ApiResponse> deletePost(@PathVariable Long postId) {
+    public ApiResponse deletePost(@PathVariable Long postId) {
         postCommandService.deletePost(postId);
-        return ResponseEntity.ok(ApiResponse.success("삭제가 성공했습니다"));
+        return ApiResponse.success("삭제가 성공했습니다");
     }
 
     /**
      * 게시물 숨기기
      */
     @PatchMapping("/{postId}/hide")
-    public ResponseEntity<ApiResponse> hidePost(@PathVariable Long postId) {
+    public ApiResponse hidePost(@PathVariable Long postId) {
         postCommandService.hidePost(postId);
-        return ResponseEntity.ok(ApiResponse.success("숨기기 성공"));
+        return ApiResponse.success("숨기기 성공");
     }
 
     /**
      * 게시물 숨김 해제
      */
     @PatchMapping("{postId}/unhide")
-    public ResponseEntity<ApiResponse> unhidePost(@PathVariable Long postId) {
+    public ApiResponse unhidePost(@PathVariable Long postId) {
         postCommandService.unhidePost(postId);
-        return ResponseEntity.ok(ApiResponse.success("숨김취소 성공"));
+        return ApiResponse.success("숨김취소 성공");
     }
 }
