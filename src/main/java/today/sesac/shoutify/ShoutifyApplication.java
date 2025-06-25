b53cfed7c331af830538531bd92a8b27adb02c40
minseokPark <qkralstjr.dev@gmail.com>
2025-06-25 20:55:13 +0900

    [#9]Feat(AI) : gemini AI 연동 및 프롬프트 구조화

--- src/main/java/today/sesac/shoutify/ShoutifyApplication.java
+++ src/main/java/today/sesac/shoutify/ShoutifyApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ShoutifyApplication {
 
-  public static void main(String[] args) {
-    SpringApplication.run(ShoutifyApplication.class, args);
-  }
-
+    public static void main(String[] args) {
+        SpringApplication.run(ShoutifyApplication.class, args);
+    }
 }
