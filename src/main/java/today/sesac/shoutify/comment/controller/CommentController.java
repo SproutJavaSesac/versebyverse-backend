4cef84b2d6e3438b4430bb9a2f322fcee5dd1526
Sooamazing <honang515@gmail.com>
2025-07-06 17:00:54 +0900

    [#68]Refactor(comment): 생성 상태 코드 201로 수정

--- src/main/java/today/sesac/shoutify/comment/controller/CommentController.java
+++ src/main/java/today/sesac/shoutify/comment/controller/CommentController.java
@@ -3,6 +3,7 @@ package today.sesac.shoutify.comment.controller;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import today.sesac.shoutify.comment.dto.request.CommentCreateRequestDto;
 import today.sesac.shoutify.comment.dto.response.CommentCreateResponseDto;
@@ -35,6 +37,7 @@ public class CommentController {
      * @return 댓글 작성 응답 DTO
      */
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public ApiResponse<CommentCreateResponseDto> createComment(
             @Valid @RequestBody CommentCreateRequestDto commentCreateRequestDto,
             @PathVariable("postId") Long postId) {
