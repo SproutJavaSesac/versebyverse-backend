5722db1b3b485b8b0c3cfc1e12fa7346e4a45b6d
wosyh18 <wosyh18@naver.com>
2025-07-07 00:02:21 +0900

    [#67]chore(게시글): @RequiredArgs 삭제

--- src/main/java/today/sesac/shoutify/post/dto/response/PostSummaryResponseDto.java
+++ src/main/java/today/sesac/shoutify/post/dto/response/PostSummaryResponseDto.java
@@ -3,10 +3,8 @@ package today.sesac.shoutify.post.dto.response;
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 @Getter
-@RequiredArgsConstructor
 @AllArgsConstructor
 public class PostSummaryResponseDto {
     private Long postId;
