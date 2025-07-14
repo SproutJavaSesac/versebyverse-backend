a5b24c7731725417c2bd565934af55d4145cd7f3
Sooamazing <honang515@gmail.com>
2025-07-14 14:38:03 +0900

    [#55]Feat(scheduler): ranking에서 사용할 Scheduler Config 설정 - 따로 관리 위해 config로 설정

---
+++ src/main/java/today/sesac/shoutify/global/config/ScheduleConfig.java
@@ -0,0 +1,13 @@
+package today.sesac.shoutify.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * 스케줄링 설정을 관리합니다.
+ */
+@Configuration
+@EnableScheduling
+public class ScheduleConfig {
+
+}
