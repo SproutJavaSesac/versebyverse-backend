8e2069a3a3f1226503a47a1968fce1128c8be854
wosyh18 <wosyh18@naver.com>
2025-07-14 12:25:08 +0900

    [#63]feat(게시글): 게시글 하나의 내용을 보여주는 dto 구현

--- src/main/java/today/sesac/shoutify/post/dto/response/PostSummaryResponseDto.java
+++ src/main/java/today/sesac/shoutify/post/dto/response/PostSummaryResponseDto.java
@@ -3,17 +3,49 @@ package today.sesac.shoutify.post.dto.response;
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import today.sesac.shoutify.post.entity.Post;
 
+/**
+ * 게시글 목록 조회 시 하나의 게시글을
+ * 나타내기 위한 dto.
+ */
+//image값이 null인 경우도 있어서 image는 final 제외 -> 빌더쓰면 안 붙여도 됨
 @Getter
-@AllArgsConstructor
+@AllArgsConstructor(staticName = "of")
+@RequiredArgsConstructor(staticName = "of")
 public class PostSummaryResponseDto {
-    private Long postId;
-    private String nickname;
-    private String afterTitle;
-    private String afterContent;
-    private LocalDateTime createdAt;
-    private int reactionCount;
-    private int commentCount;
-    private String conceptType;
+
+    private final Long postId;
+
+    private final String nickname;
+
+    private final String afterTitle;
+
+    private final String afterContent;
+
+    private final LocalDateTime createdAt;
+
+    private final int reactionCount;
+
+    private final int commentCount;
+
+    private final String conceptType;
+
     private String imageUrl;
+
+    /**
+     * Service에서 Post객체를 Dto 객체로 바꾸기 위한 정적 메소드
+     * Concept enum타입을 문자열로 변환.
+     *
+     * @param post          Post 객체
+     * @param reactionCount 반응 갯수(현재 0으로 고정)
+     * @param commentCount  댓글 갯수
+     * @return Page 객체 내부에 들어가는 Dto객체
+     */
+    public static PostSummaryResponseDto of(Post post, int reactionCount, int commentCount) {
+        return new PostSummaryResponseDto(post.getId(), post.getAuthor().getNickname(),
+                post.getAfterTitle(), post.getAfterContent(), post.getCreatedAt(),
+                reactionCount, commentCount, post.getConceptType().name(), post.getImageUrl());
+    }
 }
