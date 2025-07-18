420175c0552654efc352d9681ca903017e1e3b49
prac2317 <lowdy92@naver.com>
2025-07-18 09:22:25 +0900

    Feat(member): 회원 탈퇴 중 회원의 구글 아이디와 서비스 간 연결 끊기 구현

--- src/main/java/today/sesac/versebyverse/auth/controller/AuthController.java
+++ src/main/java/today/sesac/versebyverse/auth/controller/AuthController.java
@@ -3,15 +3,20 @@ package today.sesac.versebyverse.auth.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import today.sesac.versebyverse.auth.dto.response.LoginStatusResponseDto;
+import today.sesac.versebyverse.auth.service.AuthService;
 import today.sesac.versebyverse.auth.service.UserPrincipal;
 import today.sesac.versebyverse.global.response.ApiResponse;
 import today.sesac.versebyverse.member.entity.Member;
 import today.sesac.versebyverse.member.service.MemberService;
 
+/**
+ * 로그인 등 인증/인가 기능을 담당하는 컨트롤러.
+ */
 @Slf4j
 @RestController
 @RequestMapping("/api/v1/auth")
@@ -19,6 +24,8 @@ import today.sesac.versebyverse.member.service.MemberService;
 public class AuthController {
 
     private final MemberService memberService;
+
+    private final AuthService authService;
 
     // TODO: 프론트 테스트를 위한 로그인 상태 확인용 임시 메서드 - 수정 예정
     @GetMapping("/status")
@@ -33,5 +40,19 @@ public class AuthController {
                 email);
 
         return ApiResponse.success(response);
+    }
+
+    /**
+     * 탈퇴 요청을 수행합니다.
+     *
+     * @param userPrincipal 요청한 회원의 인증 정보.
+     * @return 성공 메시지.
+     */
+    @DeleteMapping("/members/withdraw")
+    public ApiResponse<String> withdraw(@AuthenticationPrincipal UserPrincipal userPrincipal) {
+
+        authService.withdraw(userPrincipal.getName());
+
+        return ApiResponse.success("삭제완료");
     }
 }
---
+++ src/main/java/today/sesac/versebyverse/auth/service/AuthService.java
@@ -0,0 +1,88 @@
+package today.sesac.versebyverse.auth.service;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
+import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * 로그인 등 인증/인가 기능을 담당하는 서비스.
+ */
+@Slf4j
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class AuthService {
+
+
+    private final OAuth2AuthorizedClientService authorizedClientService;
+
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    /**
+     * 탈퇴 요청을 수행합니다.
+     *
+     * @param username 사용자를 구분하기 위해 사용되는 이름입니다.
+     */
+    public void withdraw(String username) {
+
+        // 1. 현재 세션에서 액세스 토큰 가져오기
+        OAuth2AuthorizedClient client = authorizedClientService
+                .loadAuthorizedClient("google", username);
+
+        // access 토큰, refresh 토큰 확인하기
+        // TODO: refreshToken 문제 해결하기 - 로그인 후 시간 경과되면 탈퇴 안 될 것으로 추정
+        OAuth2AccessToken accessToken = client.getAccessToken();
+//        OAuth2RefreshToken refreshToken = client.getRefreshToken();
+//        if (refreshToken == null) {
+//            throw new WithdrawFailureException("refreshToken", "refreshToken 존재하지 않음");
+//        }
+
+        // 2. 구글 연동 해제 API 호출
+        revokeGoogleAccess(accessToken);
+
+        // TODO: 3. DB에서 회원 삭제
+
+        // TODO: 이후 처리
+    }
+
+    /**
+     * TODO: 카카오나 다른 소셜 로그인이 추가되면 어떻게 변경할지 고민하기
+     * 회원의 구글 아이디와 서비스의 연동을 해제하는 메서드입니다.
+     *
+     * @param accessToken 엑세스 토큰이 필요합니다.
+     */
+    public void revokeGoogleAccess(OAuth2AccessToken accessToken) {
+        String revokeUrl = "https://oauth2.googleapis.com/revoke";
+
+        // 헤더 설정: application/x-www-form-urlencoded
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        // TODO: restTemplate로 http 메세지 보내는 방식 더 알아보기
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("token", accessToken.getTokenValue());
+
+        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(params, headers);
+
+        try {
+            ResponseEntity<String> response = restTemplate.postForEntity(revokeUrl, request,
+                    String.class);
+            log.info("Google token revoked successfully: {}", response.getStatusCode());
+        } catch (Exception e) { //TODO: Exception으로 묶는 게 맞는지 체크
+            log.error("Failed to revoke Google token: {}", e.getMessage());
+        }
+    }
+
+
+}
