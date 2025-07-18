ef9c4c10682fc8ae49941276925550bf63e00151
Sooamazing <honang515@gmail.com>
2025-07-18 12:07:10 +0900

    [#18]Chore(rank): merge 병합 시 형식 맞지 않아 서식 맞춰 줌.

--- src/main/java/today/sesac/versebyverse/post/repository/PostRepository.java
+++ src/main/java/today/sesac/versebyverse/post/repository/PostRepository.java
@@ -1,9 +1,9 @@
 package today.sesac.versebyverse.post.repository;
 
-import java.util.Optional;
 import jakarta.persistence.Tuple;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -36,6 +36,7 @@ public interface PostRepository extends JpaRepository<Post, Long> {
             LocalDateTime startDate,
             LocalDateTime endDate
     );
+
     /**
      * 게시물 ID로 게시물을 조회합니다. 삭제되지 않고, 신고되지 않았으며, 숨겨지지 않은 게시물만 조회합니다.
      *
