522b8146b55e29b3260a7ed348c3ab99c79886f4
wosyh18 <wosyh18@naver.com>
2025-08-01 22:31:40 +0900

    [#128]Feat(반응하기): 반응별 갯수 조회 메서드 추가

---
+++ src/main/java/today/sesac/versebyverse/reaction/repository/ReactionRepository.java
@@ -0,0 +1,29 @@
+package today.sesac.versebyverse.reaction.repository;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import today.sesac.versebyverse.reaction.entity.Reaction;
+
+@Repository
+public interface ReactionRepository extends JpaRepository<Reaction, Long> {
+    /**
+     * 게시글 id에 대한 반응별 갯수 조회.
+     *
+     * @param postId 게시글 id
+     * @return 반응 type 별 갯수
+     */
+    @Query("""
+                    select r.type, count(r) from Reaction r where r.post.id = :postId group by r.type
+            """)
+    List<Object[]> countReactionsByPostId(@Param("postId") Long postId);
+
+//    @Query("""
+//                    select c.type, count(c) from Comment c where c.comments.id = :commnetId group by c.type
+//            """)
+//    List<Object[]> countReactionsByCommentId(@Param("commentId") Long commentId);
+
+
+}
