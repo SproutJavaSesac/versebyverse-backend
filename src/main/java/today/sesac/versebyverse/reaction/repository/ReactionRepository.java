a32367384562be2b1d0a1c7e617e3b2fd8ac487b
wosyh18 <wosyh18@naver.com>
2025-08-05 15:51:27 +0900

    [#128]Feat(반응하기): 게시물, 회원 id 와 감정을 조회하는 메서드 추가

--- src/main/java/today/sesac/versebyverse/reaction/repository/ReactionRepository.java
+++ src/main/java/today/sesac/versebyverse/reaction/repository/ReactionRepository.java
@@ -1,10 +1,13 @@
 package today.sesac.versebyverse.reaction.repository;
 
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import today.sesac.versebyverse.global.domain.Emotion;
+import today.sesac.versebyverse.post.entity.Post;
 import today.sesac.versebyverse.reaction.entity.Reaction;
 
 @Repository
@@ -31,5 +34,16 @@ public interface ReactionRepository extends JpaRepository<Reaction, Long> {
             """)
     List<Object[]> countReactionsByCommentId(@Param("commentId") Long commentId);
 
+    List<Reaction> post(Post post);
+
+    /**
+     * 게시글과 회원 id, 감정 타입을 기반으로 삭제할 emotion의 id 조회
+     *
+     * @param memberId
+     * @param postId
+     * @param emotion
+     * @return 삭제할 id의 optional 객체
+     */
+    Optional<Reaction> findByMemberIdAndPostIdAndType(Long memberId, Long postId, Emotion emotion);
 
 }
