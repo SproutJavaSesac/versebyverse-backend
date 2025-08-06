4bd294b76d1490aaf8b5703e308e1fc47ac7621f
wosyh18 <wosyh18@naver.com>
2025-08-06 15:52:27 +0900

    [#128]Feat(반응하기): 게시글 반응 수정 서비스 구현, 겹치는 로직 메서드로 분리

--- src/main/java/today/sesac/versebyverse/reaction/service/PostReactionService.java
+++ src/main/java/today/sesac/versebyverse/reaction/service/PostReactionService.java
@@ -33,6 +33,86 @@ public class PostReactionService {
      */
     public PostReactionResponseDto addPostReaction(PostReactionRequestDto postReactionRequestDto,
                                                    Long postId, Long memberId) {
+
+        savePostReaction(memberId, postId, postReactionRequestDto);
+
+        return ReactionUtils.addCountbyReactionType(postReactionRequestDto.getType(), postId, POST,
+                reactionRepository);
+    }
+
+    /**
+     * 게시글 반응 삭제하기
+     *
+     * @param type     감정 타입
+     * @param postId   게시물 id
+     * @param memberId 회원 id
+     */
+    @Transactional
+    public PostReactionResponseDto deletePostReaction(String type, Long postId, Long memberId) {
+
+        //type을 대문자로 변환
+        Emotion emotion = Emotion.valueOf(type.toUpperCase());
+
+        //해당 postId에 회원이 눌렀던 감정을 삭제
+        deletePostReactionIfExists(postId, memberId, emotion);
+
+        return ReactionUtils.addCountbyReactionType(emotion, postId, POST, reactionRepository);
+    }
+
+    /**
+     * 게시물 반응 수정하기
+     *
+     * @param postReactionRequestDto 수정된 감정 타입
+     * @param postId                 게시물 id
+     * @param memberId               회원 id
+     */
+    @Transactional
+    public PostReactionResponseDto updatePostReaction(PostReactionRequestDto postReactionRequestDto,
+                                                      Long postId, Long memberId) {
+
+        //1.회원이 기존에 눌렀던 감정 삭제
+        Optional<Reaction>
+                existingReaction = reactionRepository.findByMember_IdAndPost_Id(memberId, postId);
+
+        if (existingReaction.isPresent()) {
+            reactionRepository.deleteById(existingReaction.get().getId());
+            reactionRepository.flush();
+        }
+
+        //2.해당 postId에 memberId가 추가한 감정 삽입
+        savePostReaction(memberId, postId, postReactionRequestDto);
+
+        return ReactionUtils.addCountbyReactionType(postReactionRequestDto.getType(), postId, POST,
+                reactionRepository);
+    }
+
+    /**
+     * 게시글 감정 삭제.
+     *
+     * @param postId   게시물 id
+     * @param memberId 회원 id
+     * @param emotion  감정 타입
+     */
+    private void deletePostReactionIfExists(Long postId, Long memberId, Emotion emotion) {
+        //해당 postId와 memberId와 삭제될 reaction의 id 조회
+        Optional<Reaction> reaction =
+                reactionRepository.findByMemberIdAndPostIdAndType(memberId, postId, emotion);
+
+        //해당 감정 삭제, 이미 없으면 무시
+        if (reaction.isPresent()) {
+            reactionRepository.deleteById(reaction.get().getId());
+        }
+    }
+
+    /**
+     * 게시글 감정 추가
+     *
+     * @param memberId               회원 id
+     * @param postId                 게시글 id
+     * @param postReactionRequestDto 추가될 감정 dto
+     */
+    private void savePostReaction(Long memberId, Long postId,
+                                  PostReactionRequestDto postReactionRequestDto) {
         //1.작성자 정보 member 객체로 가져오기 (현재 사용자는 id=1로 하드코딩)
         Member author = memberService.getMember(memberId);
 
@@ -45,29 +125,10 @@ public class PostReactionService {
         //3. dto 내의 emotion 객체의 감정 꺼내기
         Emotion emotion = postReactionRequestDto.getType();
 
-        //3.dto로 온 감정에 대해 postId에 대한 author의 반응 추가하기
+        //4.dto로 온 감정에 대해 postId에 대한 author의 반응 추가하기
         Reaction reaction = Reaction.createPostReaction(author, post, emotion);
 
-        //4. reaction 객체 저장
+        //5. reaction 객체 저장
         reactionRepository.save(reaction);
-
-        return ReactionUtils.addCountbyReactionType(emotion, postId, POST, reactionRepository);
-    }
-
-    @Transactional
-    public PostReactionResponseDto deletePostReaction(String type, Long postId, Long memberId) {
-
-        //type을 대문자로 변환
-        Emotion emotion = Emotion.valueOf(type.toUpperCase());
-
-        //해당 postId와 memberId와 삭제될 reaction의 id 조회
-        Optional<Reaction> reaction =
-                reactionRepository.findByMemberIdAndPostIdAndType(memberId, postId, emotion);
-
-        //해당 감정 삭제, 이미 없으면 무시
-        if (reaction.isPresent()) {
-            reactionRepository.deleteById(reaction.get().getId());
-        }
-        return ReactionUtils.addCountbyReactionType(emotion, postId, POST, reactionRepository);
     }
 }
