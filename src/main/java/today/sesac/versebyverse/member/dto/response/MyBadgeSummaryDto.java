a379bd9a4b0753b9090e4a885e85dffdc9f2092d
prac2317 <lowdy92@naver.com>
2025-08-15 14:29:14 +0900

    [#151]Feat(member): 내 배지 조회 기능 구현

---
+++ src/main/java/today/sesac/versebyverse/member/dto/response/MyBadgeSummaryDto.java
@@ -0,0 +1,29 @@
+package today.sesac.versebyverse.member.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * 사용자가 보유한 배지 응답 DTO.
+ */
+@Getter
+@AllArgsConstructor
+public class MyBadgeSummaryDto {
+
+    private Long badgeId;
+
+    private String badgeName;
+
+    private String badgeDescription;
+
+    /**
+     * 사용자가 보유한 배지 DTO를 반환합니다.
+     *
+     * @param badgeName 배지의 이름
+     * @param badgeDescription 배지 상세 설명
+     * @return 사용자가 보유한 배지 DTO
+     */
+    public static MyBadgeSummaryDto of(Long badgeId, String badgeName, String badgeDescription) {
+        return new MyBadgeSummaryDto(badgeId, badgeName, badgeDescription);
+    }
+}
