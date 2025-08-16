c6cdab7d6f7e73ed9cc1b956e1b5e62a36b672d3
wosyh18 <wosyh18@naver.com>
2025-08-16 20:31:29 +0900

    [#128]Feat(반응하기): Patch를 Put 으로 변경

--- src/main/java/today/sesac/versebyverse/reaction/controller/CommentReactionController.java
+++ src/main/java/today/sesac/versebyverse/reaction/controller/CommentReactionController.java
@@ -4,9 +4,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -87,7 +87,7 @@ public class CommentReactionController {
      * @param commentId          댓글 id
      * @param reactionRequestDto 수정한 댓글 dto
      */
-    @PatchMapping
+    @PutMapping
     public ApiResponse<ReactionResponseDto> updateCommentReaction(@PathVariable Long postId,
             @PathVariable Long commentId, @RequestBody ReactionRequestDto reactionRequestDto,
             @AuthenticationPrincipal UserPrincipal userPrincipal) {
--- src/main/java/today/sesac/versebyverse/reaction/controller/PostReactionController.java
+++ src/main/java/today/sesac/versebyverse/reaction/controller/PostReactionController.java
@@ -4,9 +4,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -84,7 +84,7 @@ public class PostReactionController {
      * @param postId             게시물 id
      * @param reactionRequestDto 추가될 감정 dto
      */
-    @PatchMapping("/reactions")
+    @PutMapping("/reactions")
     public ApiResponse<ReactionResponseDto> updateEmotion(
             @PathVariable Long postId,
             @RequestBody ReactionRequestDto reactionRequestDto,
