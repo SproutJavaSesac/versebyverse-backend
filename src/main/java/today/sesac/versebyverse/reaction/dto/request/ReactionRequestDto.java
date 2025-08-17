c889404130fe21208ab0fd35d0f7e6252f5f4e03
wosyh18 <wosyh18@naver.com>
2025-08-17 10:46:29 +0900

    [#128]Feat(반응하기): dto 클래스를 record 로 변경

--- src/main/java/today/sesac/versebyverse/reaction/dto/request/ReactionRequestDto.java
+++ src/main/java/today/sesac/versebyverse/reaction/dto/request/ReactionRequestDto.java
@@ -1,14 +1,12 @@
 package today.sesac.versebyverse.reaction.dto.request;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 import today.sesac.versebyverse.global.domain.Emotion;
 
 /**
- * 게시글, 댓글 요청 dto
+ * 게시글, 댓글 요청 dto.
  */
-@Getter
-@AllArgsConstructor
-public class ReactionRequestDto {
-    private final Emotion type;
+public record ReactionRequestDto(
+        Emotion type
+) {
+
 }
