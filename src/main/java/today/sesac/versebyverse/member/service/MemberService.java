91e144908270bbf5d086e69365e6519dcc18ac81
prac2317 <lowdy92@naver.com>
2025-08-21 09:50:08 +0900

    [#174]Refactor(member): memberService에서 마이페이지관련 메서드 밑에 다른 회원 정보관련 메서드 오도록 순서 변경

--- src/main/java/today/sesac/versebyverse/member/service/MemberService.java
+++ src/main/java/today/sesac/versebyverse/member/service/MemberService.java
@@ -257,6 +257,41 @@ public class MemberService {
                 .toList();
     }
 
+    /**
+     * 대상 회원이 작성한 전체 게시글을 페이지네이션 방식으로 조회합니다.
+     *
+     * @param memberId 회원 ID
+     * @param pageable 페이지네이션 정보
+     * @return 회원이 작성한 게시글 목록 응답 DTO
+     */
+    public MemberPostListResponseDto getMemberPosts(Long memberId, Pageable pageable) {
+
+        validateMemberActiveExists(memberId);
+
+        // 작성한 게시글을 Page 객체으로 조회
+        Page<Post> pageByAuthorIdWithPageable = postRepository
+                .findByAuthorIdAndIsDeletedFalseAndIsBlockedFalseAndIsHiddenFalseOrderByCreatedAtDesc(
+                        memberId, pageable);
+
+        List<MemberPostSummaryDto> postSummaries = convertPostsToMemberSummaries(pageByAuthorIdWithPageable);
+
+        PaginationDto paginationDto = getPaginationDto(pageByAuthorIdWithPageable);
+
+        // Page 객체를 DTO로 변환
+        return MemberPostListResponseDto.of(
+                postSummaries,
+                paginationDto
+        );
+    }
+
+    private List<MemberPostSummaryDto> convertPostsToMemberSummaries(Page<Post> pagePosts) {
+        return pagePosts.getContent().stream()
+                .map(post -> {
+                    int commentCount = commentRepository.countByPostIdAndIsDeletedFalse(post.getId());
+                    return MemberPostSummaryDto.of(post, commentCount);
+                })
+                .toList();
+    }
 
     /**
      * 대상 회원이 작성한 전체 댓글을 페이지네이션 방식으로 조회합니다.
@@ -322,39 +357,5 @@ public class MemberService {
         }
     }
 
-    /**
-     * 대상 회원이 작성한 전체 게시글을 페이지네이션 방식으로 조회합니다.
-     *
-     * @param memberId 회원 ID
-     * @param pageable 페이지네이션 정보
-     * @return 회원이 작성한 게시글 목록 응답 DTO
-     */
-    public MemberPostListResponseDto getMemberPosts(Long memberId, Pageable pageable) {
 
-        validateMemberActiveExists(memberId);
-
-        // 작성한 게시글을 Page 객체으로 조회
-        Page<Post> pageByAuthorIdWithPageable = postRepository
-                .findByAuthorIdAndIsDeletedFalseAndIsBlockedFalseAndIsHiddenFalseOrderByCreatedAtDesc(
-                        memberId, pageable);
-
-        List<MemberPostSummaryDto> postSummaries = convertPostsToMemberSummaries(pageByAuthorIdWithPageable);
-
-        PaginationDto paginationDto = getPaginationDto(pageByAuthorIdWithPageable);
-
-        // Page 객체를 DTO로 변환
-        return MemberPostListResponseDto.of(
-                postSummaries,
-                paginationDto
-        );
-    }
-
-    private List<MemberPostSummaryDto> convertPostsToMemberSummaries(Page<Post> pagePosts) {
-        return pagePosts.getContent().stream()
-                .map(post -> {
-                    int commentCount = commentRepository.countByPostIdAndIsDeletedFalse(post.getId());
-                    return MemberPostSummaryDto.of(post, commentCount);
-                })
-                .toList();
-    }
 }
