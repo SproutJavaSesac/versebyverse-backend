5ce752442df9cedda8aec895f8b0ac8cef760481
qkralstjr <qkralstjr.dev@gmail.com>
2025-08-28 16:27:28 +0900

    [#203]Chore(게시글): FileUploadException TODO 주석 추가

--- src/main/java/today/sesac/versebyverse/global/exception/FileUploadException.java
+++ src/main/java/today/sesac/versebyverse/global/exception/FileUploadException.java
@@ -5,6 +5,7 @@ package today.sesac.versebyverse.global.exception;
  */
 public class FileUploadException extends RuntimeException {
 
+    // TODO: 도메인 분류 고려, 커스텀 예외 처리 하기
     public FileUploadException(String message) {
 
         super(message);
