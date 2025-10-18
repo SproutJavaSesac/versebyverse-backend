34461ea0145d6ebec2c1553612bc6ec35a09bf3f
prac2317 <lowdy92@naver.com>
2025-10-19 06:05:22 +0900

    [#219]Refactor(badge): 불필요한 레디스 중복 설정 삭제

--- src/main/java/today/sesac/versebyverse/global/config/RedisConfig.java
+++ src/main/java/today/sesac/versebyverse/global/config/RedisConfig.java
@@ -1,11 +1,8 @@
 package today.sesac.versebyverse.global.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -16,29 +13,6 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class RedisConfig {
 
-    @Value("${spring.data.redis.host}")
-    private String host;
-
-    @Value("${spring.data.redis.port}")
-    private int port;
-
-    @Value("${spring.data.redis.password:}")
-    private String password;
-
-    /**
-     * Redis 연결 팩토리를 생성하고 구성합니다.
-     *
-     * @return Redis 서버와의 연결을 관리하는 RedisConnectionFactory 인스턴스
-     */
-//    private RedisConnectionFactory redisConnectionFactory() {
-//
-//        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
-//        config.setHostName(host);
-//        config.setPort(port);
-//        config.setPassword(password);
-//
-//        return new LettuceConnectionFactory(config);
-//    }
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
 
